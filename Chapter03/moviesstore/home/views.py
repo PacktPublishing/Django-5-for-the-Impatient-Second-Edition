bb35a4fe8b6a118bd2426cacc68e5a7e16c4214f
Daniel Correa <yo@danielgara.com>
2024-03-29 10:34:50 -0500

    imp: C03

--- Chapter03/moviesstore/home/views.py
+++ Chapter03/moviesstore/home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'Movies Store'
+    return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
-    return render(request, 'home/about.html')
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'home/about.html', {'template_data': template_data})
