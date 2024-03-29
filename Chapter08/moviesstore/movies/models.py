e02a5d468419e71f689b8b15563f6bcae00009b7
Daniel Correa <yo@danielgara.com>
2024-03-29 12:14:45 -0500

    imp: pre-C08

---
+++ Chapter08/moviesstore/movies/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='movie_images/')
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
