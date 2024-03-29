22d9840e3f1e8986a177078617f4d8825de21e99
Daniel Correa <yo@danielgara.com>
2024-03-29 10:51:40 -0500

    imp: C06

--- Chapter06/moviesstore/movies/views.py
+++ Chapter06/moviesstore/movies/views.py
@@ -1,34 +1,22 @@
 from django.shortcuts import render
-
-movies = [
-    {
-        'id': 1, 'name': 'Inception', 'price': 12,
-        'description': 'A mind-bending heist thriller.'
-    },
-    {
-        'id': 2, 'name': 'Avatar', 'price': 13,
-        'description': 'A journey to a distant world and the battle for resources.'
-    },
-    {
-        'id': 3, 'name': 'The Dark Knight', 'price': 14,
-        'description': 'Gothams vigilante faces the Joker.'
-    },
-    {
-        'id': 4, 'name': 'Titanic', 'price': 11,
-        'description': 'A love story set against the backdrop of the sinking Titanic.',
-    },
-]
+from .models import Movie
 
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+
     template_data = {}
     template_data['title'] = 'Movies'
     template_data['movies'] = movies
     return render(request, 'movies/index.html', {'template_data': template_data})
 
 def show(request, id):
-    movie = movies[id - 1]
+    movie = Movie.objects.get(id=id)
 
     template_data = {}
-    template_data['title'] = movie['name']
+    template_data['title'] = movie.name
     template_data['movie'] = movie
     return render(request, 'movies/show.html', {'template_data': template_data})
