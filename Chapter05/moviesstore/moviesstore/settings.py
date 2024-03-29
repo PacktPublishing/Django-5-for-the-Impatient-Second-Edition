aa519cbbf2285ad890ccd3f75f92a6702f3a9a42
Daniel Correa <yo@danielgara.com>
2024-03-29 10:46:07 -0500

    imp: C05

--- Chapter05/moviesstore/moviesstore/settings.py
+++ Chapter05/moviesstore/moviesstore/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'moviesstore/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Chapter05/moviesstore/moviesstore/urls.py
+++ Chapter05/moviesstore/moviesstore/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
