48b6d434a136618d73c4353d8f825e3f75806322
Daniel Correa <yo@danielgara.com>
2024-03-30 12:58:36 -0500

    imp: C08

---
+++ Chapter08/moviesstore/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup', views.signup, name='accounts.signup'),
+    path('login/', views.login, name='accounts.login'),
+    path('logout/', views.logout, name='accounts.logout'),
+]
--- Chapter08/moviesstore/moviesstore/urls.py
+++ Chapter08/moviesstore/moviesstore/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
