bf25c9029230298212c8587f41d7c0e0c019ec16
Daniel Correa <yo@danielgara.com>
2024-03-30 12:59:07 -0500

    imp: pre-C09

---
+++ Chapter09/moviesstore/movies/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Movie
+
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
---
+++ Chapter09/moviesstore/movies/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='movies.index'),
+    path('<int:id>/', views.show, name='movies.show'),
+]
